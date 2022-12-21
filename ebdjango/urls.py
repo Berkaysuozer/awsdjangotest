af4dca46e1c3315bd4b0c0458ba5e6ab7f372104
Berkay Şuözer <berkaysuozer@Berkay-MacBook-Pro.local>
2022-12-22 00:57:28 +0300

    app added

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -1,7 +1,7 @@
-"""ebdjango URL Configuration
+"""djangoApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt import views
+from register.views import UserCreate, UserDetail
 
 urlpatterns = [
+    path('', include('members.urls')),
+    path('', include('apis.urls')),
     path('admin/', admin.site.urls),
+    path("api/auth/register/", UserCreate.as_view(), name="register"),
+    path("api/auth/me/", UserDetail.as_view(), name="me"),
+    path(
+        "api/auth/obtain/token/",
+        views.TokenObtainPairView.as_view(),
+        name="obtain-token",
+    ),
+    path(
+        "api/auth/obtain/refreh/",
+        views.TokenRefreshView.as_view(),
+        name="refresh-token",
+    )    
 ]
